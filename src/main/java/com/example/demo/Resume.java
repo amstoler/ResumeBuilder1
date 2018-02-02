975732e7c1bce962e4bc9760701fca05387510c4
amstoler <amstoler@gmail.com>
2018-02-02 11:01:09 -0500

    Added more fields to private variables and for more text to display to webpage.

--- src/main/java/com/example/demo/Resume.java
+++ src/main/java/com/example/demo/Resume.java
@@ -14,8 +14,24 @@ public class Resume {
     private long id;
 
     @NotNull
-    @Size(min=4)
+    @Size(min=1)
     private String name;
+
+    @NotNull
+    @Size(min=3) //only 3 necessary. Email has 3 parts name@domain
+    private String email;
+
+    @NotNull
+    @Size(min=5)
+    private String education;
+
+    @NotNull
+    @Size(min=5)
+    private String experience;
+
+    @NotNull
+    @Size
+    private String skills;
 
     public Resume() {
     }
@@ -34,5 +50,37 @@ public class Resume {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public String getExperience() {
+        return experience;
+    }
+
+    public void setExperience(String experience) {
+        this.experience = experience;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
     }
 }
