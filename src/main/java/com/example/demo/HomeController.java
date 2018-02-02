e1248707b9eb467cae88884801eec5509be03c6b
Ariel Stoler <amstoler@gmail.com>
2018-02-01 23:00:18 -0500

    Change needed in MainController to Resumes did not save first time.

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -3,7 +3,12 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class HomeController {
@@ -13,7 +18,24 @@ public class HomeController {
 
     @RequestMapping("/")
     public String listResumes(Model model){
-        model.addAttribute("resume", resumeRepository.findAll() );
+        model.addAttribute("resumes", resumeRepository.findAll() );
         return "list";
     }
+
+    @GetMapping("/add")
+    public String resumeform(Model model){
+        model.addAttribute("resume", new Resume());
+        return "resumeform";
+    }
+
+    @PostMapping("/process")
+    public String processForm(@Valid Resume resume, BindingResult result)
+    {
+        if (result.hasErrors()){
+            return "resumeform";
+        }
+        resumeRepository.save(resume);
+        return "redirect:/";
+    }
 }
+
